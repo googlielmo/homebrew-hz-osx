1955aab6e43b1241a70dff3123e9ee88bf8ef4bd
Guglielmo Nigri <googlielmo@gmail.com>
2018-03-02 17:34:58 +0100

    Revision 4

--- hazelcast-member.rb
+++ hazelcast-member.rb
@@ -2,9 +2,9 @@ class HazelcastMember < Formula
   desc "A command line tool for running one or more Hazelcast member instance(s) on the local machine"
   homepage "https://github.com/googlielmo/hazelcast-member"
   url "https://github.com/googlielmo/hazelcast-member/raw/master/archive/hazelcast-member-3.9.2.tar.gz"
-  sha256 "dd6209db0e12af1eb4e6cfc2dcdda661d3617e06ff2ca0e2218e3d9bb5e840f8"
+  sha256 "17f5ca508976dced1c6cbca08b0552c5ebde19597b9c85ec2c6450105b4d6bae"
   version "3.9.2"
-  revision 3
+  revision 4
 
   depends_on "coreutils"
   depends_on :java => "1.7+"
@@ -15,7 +15,7 @@ class HazelcastMember < Formula
     libexec.install Dir["*"]
     bin.install_symlink Dir["#{libexec}/bin/hazelcast-member"]
     var.mkpath
-    inreplace "#{libexec}/bin/env.sh", "${var_run}", "#{var}/run"
+    inreplace "#{libexec}/bin/pid-utils.sh", "${var_run}", "#{var}/run"
     inreplace "#{libexec}/bin/start.sh", "${var_run}", "#{var}/run"
     prefix.install_metafiles
   end
