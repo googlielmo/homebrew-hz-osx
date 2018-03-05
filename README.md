5a0e7d35b08f26679e899330fe74eb04518b9a62
Guglielmo Nigri <googlielmo@gmail.com>
2018-03-05 19:06:32 +0100

    Revision 5. Add README.md contents

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# homebrew-hz-osx
+# A Homebrew repository
+
+**WARNING** Work in progress. Use at your own risk.
+
+## About
+
+This repository contains the Homebrew formula for `hazelcast-member`.
+
+## How to install
+
+    $ brew tap googlielmo/homebrew-hz-osx
+    $ brew install hazelcast-member
+
+### To clean up everything (may come in handy):
+
+    $ brew uninstall hazelcast-member
+    $ brew untap googlielmo/homebrew-hz-osx
+    $ brew cleanup -s
--- hazelcast-member.rb
+++ hazelcast-member.rb
@@ -2,9 +2,9 @@ class HazelcastMember < Formula
   desc "A command line tool for running one or more Hazelcast member instance(s) on the local machine"
   homepage "https://github.com/googlielmo/hazelcast-member"
   url "https://github.com/googlielmo/hazelcast-member/raw/master/archive/hazelcast-member-3.9.2.tar.gz"
-  sha256 "17f5ca508976dced1c6cbca08b0552c5ebde19597b9c85ec2c6450105b4d6bae"
+  sha256 "daa57b4fe54e5b4dccec678ad625d106c844af12089ea6294f87291890b6aae8"
   version "3.9.2"
-  revision 4
+  revision 5
 
   depends_on "coreutils"
   depends_on :java => "1.7+"
@@ -15,8 +15,7 @@ class HazelcastMember < Formula
     libexec.install Dir["*"]
     bin.install_symlink Dir["#{libexec}/bin/hazelcast-member"]
     var.mkpath
-    inreplace "#{libexec}/bin/pid-utils.sh", "${var_run}", "#{var}/run"
-    inreplace "#{libexec}/bin/start.sh", "${var_run}", "#{var}/run"
+    inreplace "#{libexec}/bin/utils.sh", "${var}", "#{var}"
     prefix.install_metafiles
   end
 end
