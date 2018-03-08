1d97a56debb5169d7194989938d2bd2d5b44b331
Guglielmo Nigri <googlielmo@gmail.com>
2018-03-08 15:51:12 +0100

    Revision 7

--- hazelcast-member.rb
+++ hazelcast-member.rb
@@ -2,9 +2,9 @@ class HazelcastMember < Formula
   desc "A command line tool for running one or more Hazelcast member instance(s) on the local machine"
   homepage "https://github.com/googlielmo/hazelcast-member"
   url "https://github.com/googlielmo/hazelcast-member/raw/master/archive/hazelcast-member-3.9.2.tar.gz"
-  sha256 "5b388276f268ff959285b3de24d79683fbf4c70427cee199fd8c7bc852454af2"
+  sha256 "d6698986b77dde307b594a420280c8d96bceadde54b2aba7f8df6c3255962a20"
   version "3.9.2"
-  revision 6
+  revision 7
 
   depends_on "coreutils"
   depends_on :java => "1.7+"
@@ -17,5 +17,13 @@ class HazelcastMember < Formula
     var.mkpath
     inreplace "#{libexec}/bin/utils.sh", "${var}", "#{var}"
     prefix.install_metafiles
+    bash_completion.install "#{libexec}/bin/hazelcast-member-completion.sh" => "hazelcast-member"
+  end
+
+  def caveats
+    <<-EOS
+To enable tab completion add to your profile:
+  if which hazelcast-member > /dev/null; then eval "$(hazelcast-member init -)"; fi
+    EOS
   end
 end
