eb6fa01cb1b0d3d887b7762365bc9a87f55e9976
Guglielmo Nigri <googlielmo@gmail.com>
2018-03-12 18:40:31 +0100

    Version 3.9.3_1

--- hazelcast-member.rb
+++ hazelcast-member.rb
@@ -1,10 +1,10 @@
 class HazelcastMember < Formula
   desc "A command line tool for running one or more Hazelcast member instance(s) on the local machine"
   homepage "https://github.com/googlielmo/hazelcast-member"
-  url "https://github.com/googlielmo/hazelcast-member/raw/master/archive/hazelcast-member-3.9.2.tar.gz"
-  sha256 "97d3fba0cb322bf21bf3cb1356db4a68b1f4b9974f7987dd066353a8a2bce3f2"
-  version "3.9.2"
-  revision 8
+  version "3.9.3"
+  revision 1
+  url "https://github.com/googlielmo/hazelcast-member/raw/master/archive/hazelcast-member-#{version}_#{revision}.tar.gz"
+  sha256 "88cf19ba778d32ab5d2859b89a49fd6e45f7a9c301b759a2ffa1419220026bc5"
 
   depends_on "coreutils"
   depends_on :java => "1.7+"
@@ -12,10 +12,13 @@ class HazelcastMember < Formula
   bottle :unneeded
 
   def install
+    mkpath "#{etc}/hazelcast"
     libexec.install Dir["*"]
+    etc.install_symlink "#{libexec}/etc/hazelcast.xml" => "hazelcast/hazelcast.xml"
     bin.install_symlink Dir["#{libexec}/bin/hazelcast-member"]
     var.mkpath
     inreplace "#{libexec}/bin/utils.sh", "${var}", "#{var}"
+    inreplace "#{libexec}/bin/utils.sh", "${etc}", "#{etc}"
     prefix.install_metafiles
     bash_completion.install "#{libexec}/bin/hazelcast-member-completion.sh" => "hazelcast-member"
   end
